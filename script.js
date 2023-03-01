5d5d7cc3098314fd30192d54424556ccc63bd991
Aid24 <105000149+Aid24@users.noreply.github.com>
2023-03-01 13:59:53 +0200

    add commits

---
+++ script.js
@@ -0,0 +1,19 @@
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+const key = generateKey(16, characters);
+
+function generateKey(length, characters) {
+  let result = '';
+  const charactersLength = characters.length;
+
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+    return result;
+    
+} 
+
+generateKey(length, characters);
+
+
+console.log(key);
